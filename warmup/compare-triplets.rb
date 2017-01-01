3dbe196260257006d16d4c7f5598f33713197bbe
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-01-01 11:39:09 -0200

    firstday: warmup challenges

---
+++ warmup/compare-triplets.rb
@@ -0,0 +1,11 @@
+#!/bin/ruby
+# https://www.hackerrank.com/challenges/compare-the-triplets
+
+alice = gets.strip.split(' ').map(&:to_i)
+bob = gets.strip.split(' ').map(&:to_i)
+
+result = alice.zip(bob)
+  .map{ |a,b| a==b ? [0,0] : a>b ? [1,0] : [0,1] }
+  .reduce([0,0]) { |acc, e| acc.zip(e).map{|a,e| a+e} }
+
+puts "#{result[0]} #{result[1]}"
